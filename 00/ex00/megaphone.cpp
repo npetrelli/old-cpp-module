c140591846a5e1086d3f2fa2dcb281470c984028
npetrelli <mongush.baylak17@gmail.com>
2022-04-09 15:32:35 +0300

    Add code

---
+++ 00/ex00/megaphone.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int i;
+	int j;
+	char temp;
+
+	if (argc == 1)
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	else if (argc > 2)
+	{
+		j = 0;
+		while (++j < argc)
+		{
+			i = 0;
+			while (argv[j][i])
+			{
+				temp = argv[j][i++];
+				if (temp > 96 && temp < 123)
+					temp -= 32;
+				cout << temp;
+			}
+		}
+		cout << endl;
+	}
+	return (0);
+}
